38a678c3ef7e830857b024d052308bd41478faa7
oleg <oleg.bel.sl@ya.ru>
2020-12-08 20:38:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log (number);
+
+const obj = {
+    a:50
+};
+
+obj.a = 10;
+console.log (obj);
+
+
+console.log(name);
+var name = "Oleg";
+{
+    var result = 50;
+}
+console.log (result);
+const persone = "Alex";
+const bool = false;
+console.log (something); 
